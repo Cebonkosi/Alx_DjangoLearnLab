47cbde3fcda8ea2478c1b758673410953f46369f
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-09-08 19:35:11 +0200

    missing the query that retrieves all books by a specific author.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    books = Book.objects.filter(author=author)  # ✅ This is the required line
+    return books
 
 # List all books in a library
 def get_books_in_library(library_name):
