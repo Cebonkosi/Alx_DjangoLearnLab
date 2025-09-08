e49cd1d3908995e8445913dcb1ae61bd93b1d890
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-09-08 20:19:35 +0200

    link both the function-based and class-based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView   
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("relationship_app.urls")),  # include app URLs
+    path("books/", list_books, name="list_books"),   # function-based view
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
 ]
