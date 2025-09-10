363feafc635e61b54c66105eae7297e5774d8961
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-09-10 21:49:27 +0200

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import user_passes_test
 
 # Function-based view for listing all books
 def list_books(request):
@@ -39,3 +40,27 @@ class CustomLoginView(LoginView):
 # Logout View – using Django’s built-in
 class CustomLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
+
+
+# --- Role check functions ---
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+# --- Views restricted by roles ---
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
