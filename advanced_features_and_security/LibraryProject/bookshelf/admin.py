72c871ea0084d3881fdda436a06d95e95278ed18
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-09-14 21:46:06 +0200

    passing a check 2

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,1 +1,32 @@
-# ...existing code from Introduction_to_Django/LibraryProject/bookshelf/admin.py...
+# LibraryProject/bookshelf/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    # Define fields to be displayed in the admin list
+    list_display = ('email', 'date_of_birth', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+
+    # Fieldsets for editing user details
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Fields used when creating a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'profile_photo', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+# ✅ Register CustomUser with its admin
+admin.site.register(CustomUser, CustomUserAdmin)
