cac58439963a3ed3c2aa598832fca547e24acf74
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-09-14 22:26:00 +0200

    forms file

---
+++ advanced_features_and_security/LibraryProject/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,10 @@
+# LibraryProject/bookshelf/forms.py
+
+from django import forms
+from .models import Book
+
+# Example form for creating/editing books
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
