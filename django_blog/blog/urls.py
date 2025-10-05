9425f0f8355a2256c59952148a6d6ea0a9782606
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-10-05 23:58:41 +0200

    Add URL pattern for filtering posts by tag slug

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -33,5 +33,6 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path('search/', search_posts, name='search-posts'),
+    path('tags/<slug:tag_slug>/', posts_by_tag, name='posts-by-tag'),
     path('tags/<str:tag_name>/', PostByTagListView.as_view(), name='posts_by_tag'),
 ]
