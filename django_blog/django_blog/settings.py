d7b70bc52318e9dea384fdeb6b438e62a416b3f3
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-10-05 22:44:13 +0200

    Add database configuration to settings.py with default credentials

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': 'admin',        
+        'PASSWORD': '123@admin',    
+        'HOST': '',        
+        'PORT': '',        
     }
 }
+
 
 
 # Password validation
