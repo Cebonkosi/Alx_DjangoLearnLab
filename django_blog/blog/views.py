c5ca1aea8d96f4740906be04190476d123c4aab3
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-10-05 23:42:22 +0200

    Refactor comment views: implement create and list functionality, update success URL handling

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -107,29 +107,39 @@ def add_comment(request, post_id):
         form = CommentForm()
     return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
 
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     fields = ['content']
-    template_name = 'blog/edit_comment.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
         return super().form_valid(form)
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
 
     def test_func(self):
         comment = self.get_object()
         return self.request.user == comment.author
 
-    def get_success_url(self):
-        return self.object.post.get_absolute_url()
 
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
-    template_name = 'blog/delete_comment.html'
+    success_url = reverse_lazy('blog-home')  # adjust as needed
 
     def test_func(self):
         comment = self.get_object()
         return self.request.user == comment.author
 
-    def get_success_url(self):
-        return self.object.post.get_absolute_url()
+
+class CommentListView(ListView):
+    model = Comment
+    template_name = 'blog/comment_list.html'
+    context_object_name = 'comments'
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        return Comment.objects.filter(post=post).order_by('-created_at')
